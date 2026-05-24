8eeddb78461cea1885c902ec07e875183cca1e0c
Guilherme Monção <guilherme_oliveira27@outlook.com>
2026-05-23 21:37:41 -0400

    Feat: Configuração do EF Core

--- Program.cs
+++ Program.cs
@@ -1,4 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using RangoAgilAPI.DbContexts;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<RangoDbContext>(
+    o => o.UseSqlite(builder.Configuration["ConnectionStrings"])
+);
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
